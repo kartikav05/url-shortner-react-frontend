cea7714d065d5d8b0792e4be033562a09d93d723
kartikav05 <kartikav2001@gmail.com>
2024-09-19 23:20:07 +0530

    feat: defined views for top urls and url input

---
+++ src/views/TopUrls.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UrlTable from '../components/UrlTable';
+
+const TopUrls = () => (
+  <div>
+    <UrlTable />
+  </div>
+);
+
+export default TopUrls;
---
+++ src/views/UrlInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UrlForm from '../components/UrlForm';
+
+const UrlInput = () => (
+  <div>
+    <UrlForm />
+  </div>
+);
+
+export default UrlInput;
