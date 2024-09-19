6fa3ca1c004956c7872f6e000ab8c6402c47360c
kartikav05 <kartikav2001@gmail.com>
2024-09-19 23:26:35 +0530

    feat: defined handle submit method to create the short url and handled the erros

--- src/components/UrlForm.jsx
+++ src/components/UrlForm.jsx
@@ -7,6 +7,21 @@ const UrlForm = () => {
   const [shortUrl, setShortUrl] = useState(null);
   const [error, setError] = useState(null);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(shortUrl){
+      setShortUrl(null)
+    }
+    try {
+      const response = await shortenUrl(url);
+      setShortUrl({ short_code: response.short_code, message: response.message });
+      setError(null);
+      setUrl('');
+    } catch (err) {
+      setError(err.errors);
+    }
+  };
+
   return (
     <>
       URL Form
