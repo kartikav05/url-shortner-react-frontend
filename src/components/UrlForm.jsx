209a05af2aacff8a6c06821de52b17551c1908e4
kartikav05 <kartikav2001@gmail.com>
2024-09-19 23:25:07 +0530

    feat: defined the required states for the components

--- src/components/UrlForm.jsx
+++ src/components/UrlForm.jsx
@@ -3,6 +3,9 @@ import { TextField, Button, Typography, Alert, Card } from '@mui/material';
 import { shortenUrl } from '../service';
 
 const UrlForm = () => {
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState(null);
+  const [error, setError] = useState(null);
 
   return (
     <>
