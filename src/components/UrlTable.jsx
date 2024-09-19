82e9ed15ae4cd94f0ea62a53b0eb618d10ea2632
kartikav05 <kartikav2001@gmail.com>
2024-09-19 23:38:10 +0530

    feat: added the table component with appropriate stylings and rendered the data

--- src/components/UrlTable.jsx
+++ src/components/UrlTable.jsx
@@ -12,6 +12,17 @@ const UrlTable = () => {
     };
     fetchUrls();
   }, []);
+
+  const headerCellStyle = {
+    backgroundColor: '#1976D2',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  };
+
+  const bodyCellStyle = {
+    color: '#000000'
+  };
+
   return (
     <Card
       sx={{
@@ -24,6 +35,36 @@ const UrlTable = () => {
       <Typography variant="h5" gutterBottom>
         Top 100 Most Accessed URLs
       </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={headerCellStyle}>Click Count</TableCell>
+              <TableCell sx={headerCellStyle}>Short Code</TableCell>
+              <TableCell sx={headerCellStyle}>Full URL</TableCell>
+              <TableCell sx={headerCellStyle}>Shortened URL</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {urls.map((url) => (
+              <TableRow key={url.short_code}>
+                <TableCell sx={bodyCellStyle}>{url.click_count}</TableCell>
+                <TableCell sx={bodyCellStyle}>{url.short_code}</TableCell>
+                <TableCell sx={bodyCellStyle}>
+                  <a href={url.full_url} target="_blank">
+                    {url.full_url}
+                  </a>
+                </TableCell>
+                <TableCell sx={bodyCellStyle}>
+                  <a href={`http://localhost:3000/${url.short_code}`} target="_blank">
+                    http://localhost:3000/{url.short_code}
+                  </a>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Card>
   );
 };
